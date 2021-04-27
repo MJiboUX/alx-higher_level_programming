520794eb278253c29a06b4cca83a649495cbc180
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-04-27 15:50:40 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    for j in range(0, 100):
-       print("{:d}{:d}".format(i, j))
+for num in range(0, 100):
+    if num in [99]:
+        print("{:d}".format(num))
+        break
+    print("{:02d}, ".format(num), end="")
