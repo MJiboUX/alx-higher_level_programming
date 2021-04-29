78e72289220758f7b975b2e2474c57c178817532
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-04-29 10:48:40 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,9 @@
 if __name__ == '__main__':
     import sys
     arguments = len(sys.argv) - 1
+    position = 1
     if len(sys.argv) is 1:
         print('0 arguments.')
     else:
-        print ('{} arguments'.format(arguments))
-        print ('{}'.format(str(sys.argv)))
+    while (arguments >= position):
+        print('{:d}: {:s}'.format(position, sys.argv[position]))
