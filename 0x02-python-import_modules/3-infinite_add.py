9f10757bd5bade4d889b7f9ab4ac922c21e5fd8a
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-04-29 11:26:49 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,13 +3,8 @@ import sys
 def main(*argv):
     arguments = len(sys.argv) - 1
     sum = 0
-    if arguments == 1:
-        print("{:d} argument:".format(arguments))
-    elif arguments == 0:
-        print("{:d} argument".format(arguments))
-    else:
-        print("{:d} arguments".format(arguments))
-    for args in sys.argv:
+    if arguments > 1:
+       for args in sys.argv:
             if args != sys.argv[0]:
                 sum = sum + int(args)
     print(sum)
