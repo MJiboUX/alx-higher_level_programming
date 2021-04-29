982210b96755792f812a43fc7cef1da2431cd968
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-04-29 10:43:05 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
+arguments = len(sys.argv) - 1
     import sys
     if len(sys.argv) is 1:
         print('0 arguments.')
     else:
-        print ('{} arguments'.format(len(sys.argv) - 1))
+        print ('{} arguments'.format(arguments))
         print ('{}'.format(str(sys.argv)))
