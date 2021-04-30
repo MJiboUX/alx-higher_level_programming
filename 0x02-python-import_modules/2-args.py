d78631834e25a7c5ad4e7633327363e13d5f0449
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-04-30 00:52:59 -0700

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,13 +8,12 @@ def main(*argv):
     if arguments == 1:
         print("{:d} argument:".format(arguments))
     elif arguments == 0:
-        print("{:d} argument".format(arguments))
-    else:
         print("{:d} arguments".format(arguments))
+    else:
+        print("{:d} arguments:".format(arguments))
     for args in sys.argv:
         if (position != 0):
             print("{}: {}".format(position, args))
-        position += 1
-        
+        position += 1        
 if __name__ == "__main__":
     main()
