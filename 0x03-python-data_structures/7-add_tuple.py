5da48e60c2e65c40cfec97ce071d39932bada87a
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-05-05 07:37:44 +0300

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     j = ()
-    for i in (tuple_a, tuple_b=()):
+    for i in (tuple_a, tuple_b):
         if len(i) == 0:
             i = (0, 0)
         elif len(i) == 1:
