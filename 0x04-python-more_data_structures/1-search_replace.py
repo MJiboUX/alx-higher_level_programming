8396afd6442629cda381db3e75a8c609eff0d429
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-05-06 11:29:05 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = []
-    for i in my_list
+    for i in my_list:
         if i == search:
             new.append(replace)
         else:
