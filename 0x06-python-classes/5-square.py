9b9166dfd68e4c5d7bf23d47cc3b70416feed4ac
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-05-18 14:25:01 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,29 @@
-#
+#!/usr/bin/python3
+""" defines a square with size"""
+
+
+class Square:
+    __size = 0
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+             raise ValueError('size must be >= 0')
+        self.__size = size
+        
+    def area(self):
+        A = self.__size * self.__size
+        return (A)
+      
+    @property
+    def size(self):
+        """getter def"""
+        return self.__size
+      
+    @property
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+             raise ValueError('size must be >= 0')
+        self.__size = size
