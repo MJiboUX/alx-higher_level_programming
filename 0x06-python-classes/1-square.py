5828fac0f99f60fbe91ee7e71233404e5d45c867
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-05-18 16:32:45 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-""" python3 -c 'print(__import__("my_module").Square.init)"""
+""" squaressss"""
 
 
 class Square:
-    """Square \n
-    Attributes:
-    size: square size
-    """"""
     
     def __init__(self, size):
         """Initialize class"""
