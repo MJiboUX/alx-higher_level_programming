68db80e60182031e54790f31a60ed77c4627b186
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-05-19 08:50:35 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,8 +7,7 @@ class Square:
         Attributes:
         size (int): size of square
     """
-    __size = 0
-    
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
