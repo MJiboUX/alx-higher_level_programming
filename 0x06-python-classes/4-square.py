a5f2d299ade68ce6e8f1d617f0c4290ebfa07f61
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-05-19 08:56:57 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,23 +7,24 @@ class Square:
     Attributes:
         size: size of square
     """
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
-        
+
     def area(self):
         """ return area """
         A = self.__size * self.__size
         return (A)
-    
+
     @property
     def size(self):
         """ return def """
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
