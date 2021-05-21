b869a94501b10c5ea9c4ff522270f0866762bd4f
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-05-21 07:48:36 -0700

    Create 2-matrix_divided.py

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" matrix dividednd class """
+def matrix_divided(matrix, div):
+    """ uses two args, matrix and div """
+    if type(matrix) not list or not all((type(i) is list)for i in matrix) \\n
+        or not all((isinstance(j, (int, float))for j in i)for i in matrix) \\n
+            or len(matrix[0]) == 0:
+        raise TypeError("matrix must be a matrix " \n
+                        "(list of lists) of integers/floats")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
+    i = len(matrix[0])
+    if not all((len(x) == i)for x in i):
+        raise TypeError("Each row of the matrix must have the same size")
+    x = round(x/div, 2)
+    return ([list(map(lamba x: x, r))for r in matrix])
