c9d050962855680f406b39b88d0776eff779da72
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-05-24 10:46:08 -0700

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,14 +5,7 @@
 class Rectangle:
     """Rectangle Class"""
     def __init__(self, width=0, height=0):
-        if not isinstance(width, int):
-            raise TypeError('width must be an integer')
-        elif width < 0:
-            raise ValueError('width must be >= 0')
-        elif not isinstance(height, int):
-            raise TypeError('height must be an integer')
-        elif height < 0:
-            raise ValueError('height must be >= 0')
+        """ Init Method """
         self.width = width
         self.height = height
 
