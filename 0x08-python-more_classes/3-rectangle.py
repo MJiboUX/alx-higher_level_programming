10bb74d58de88913cce205616b739c1cbc635f0b
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-05-24 11:25:53 -0700

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,4 +51,5 @@ class Rectangle:
         """define informal print str"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return (('#'*self.__width + "\n")*self.__height)[:-1]
+        else:
+            return (('#'*self.__width + "\n")*self.__height)[:-1]
