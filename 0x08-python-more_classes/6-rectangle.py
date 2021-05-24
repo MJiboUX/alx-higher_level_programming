01ff70572e1cda9f8c422e26b1272152cb81f032
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-05-24 11:40:28 -0700

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
         """ Init Method """
         self.__width = width
         self.__height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
