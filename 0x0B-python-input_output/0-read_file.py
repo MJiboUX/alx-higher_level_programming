27c4bb07fcdc66ebcdf9dc34ec73f6407e9931ef
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-01 15:11:22 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-    """ reading and writing """
+""" reading and writing """
 def read_file(filename=""):
     """ reading operation """
     with open(filename, "r", encoding = "utf-8") as f:
