495bbcfa0dff64371fcd43ea76cd2bd24cd123e4
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-02 15:12:43 +0300

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ def load_from_json_file(filename):
     """
     Load JSON  object
     """
-    with open(filename, "w", encoding = "utf-8") as f:
+    with open(filename, encoding = "utf-8") as f:
         json.load(f)
