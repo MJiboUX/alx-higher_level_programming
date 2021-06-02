306e2ee4f2aafb0cbbde9c39a4ddddf724d56be4
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-02 15:01:21 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     """ write and encoding operation """
-    with open(filename, "a", encoding = "utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         f.write(text)
     return len(text)
