02781b62d710edfe91e825e4ea4aa2e2a7dc6140
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-02 15:00:22 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,6 +8,6 @@ def write_file(filename="", text=""):
     """
     write and encoding operation
     """
-    with open(filename, "w", encoding = "utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
     return len(text)
