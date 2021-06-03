ff87ae7a14bd187df47e5a33cbbc91f6de8dcaac
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-03 19:32:20 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+""" new class called base """
 
+
+class Base:
+    """ define Base with id """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ method initialization """
+        if id is not None:
+            self.id = id
+        else:
+            inc = Base.__nb_objects += 1
+            self.id = inc
