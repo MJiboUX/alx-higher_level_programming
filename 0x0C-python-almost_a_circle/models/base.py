e8d5ec50e74945f260cf9dae51e9baf60786fb84
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-03 19:54:24 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,5 +12,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            inc = Base.__nb_objects += 1
-            self.id = inc
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
