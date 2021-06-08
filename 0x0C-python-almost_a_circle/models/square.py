52f6de00405a339875464e03c10aac4e6a4bcca8
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-08 14:29:21 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,18 +16,18 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        """
-            Size Getter
-        """
+        """ Getter """
         return self.width
 
     @size.setter
     def size(self, value):
-        """
-            Size Setter
-        """
-        self.height = value
+        """ Setter """
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
         self.width = value
+        self.height = value
 
     def __str__(self):
         """overloading __str__ method """
