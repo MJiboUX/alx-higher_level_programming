b9ac8938908f97204fc20bee84bfcc10c0924cdd
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-06-08 14:54:16 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,3 +39,12 @@ class Base:
         if not json_string:
             return []
         return json.loads(json_string)
+
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes """
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        if cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
