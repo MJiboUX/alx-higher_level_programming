591d37eb396c8381aa538159d8d7b5e5218c4f29
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-07-13 14:46:48 +0300

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 --  script that lists all cities contained in the database hbtn_0d_usa
 -- display ONLY: cities.id - cities.name - states.name,Results must be sorted in asc order by cities.id
-SELECT * FROM cities INNER JOIN states ON cities.id = states.id ORDER BY cities.id ASC
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.id = states.id ORDER BY cities.id ASC
