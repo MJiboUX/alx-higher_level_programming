4a02adc82792f0cdb4e5c633cb5d4e4a5169b3af
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-02 12:00:57 +0300

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,9 +2,10 @@
 let x = 0;
 process.argv.forEach((val, index) => {
   x++;
+  if (index === 2) {
+  console.log(`${val}`);
+}
 });
 if (x <= 2) {
   console.log('No argument');
-} else {
-  console.log(`${val}`);
 }
