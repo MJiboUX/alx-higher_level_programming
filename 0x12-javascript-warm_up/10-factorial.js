6b78c3e9bf13fdb7865ed3d730b23e31d981bac9
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-02 14:28:06 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (n == 0 || n == 1) {
-    return 1;
+  let result = 1;
+  while (n >= 1) {
+      result = result * n;
   }
-  else {
-    let result = n;
-    while (n > 1) {
-      result = result * (n - 1);
-      n = n - 1;
-    }
-    return result;
-  }
+  return (result);
 }
 console.log(factorial(parseInt(process.argv[2])));
