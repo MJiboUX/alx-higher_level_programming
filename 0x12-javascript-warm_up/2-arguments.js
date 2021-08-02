b81392ad414f6d51f39b22966530630c2b07126e
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-02 11:46:21 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
-function display(arg) {
-if (arg.length === 0) {
-console.log('No argument”')
+let x = 0;
+process.argv.forEach((_val, _index) => {
+  x++;
+});
+if (x <= 2) {
+  console.log('No argument');
+} else if (x === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-}
-display();
