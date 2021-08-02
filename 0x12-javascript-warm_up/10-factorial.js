75d1ab9f0c41228f5f48bd8bd9c9a5b744e5beec
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-02 14:17:10 +0300

    Create 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n) == true) {
+    return 1;
+  }
+  else if (n == 0 || n == 1) {
+    return 1;
+  }
+  else {
+    let result = n;
+    while (n > 1) {
+      result = result * (n - 1);
+      n = n - 1;
+    }
+    return result;
+  }
+factorial(process.argv[2]);
