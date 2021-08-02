87d053165864dbe20e57ed086ed12a482bf0aaaf
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-02 13:52:44 +0300

    Create 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const op = process.argv[2];
+if (isNaN(op) === true) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < op; i++) {
+    for (let j = 0; j < op; j++) {
+    console.log('X');
+    }
+  }
+}
