93ad774158767043b553aaec8446e7f55e6e4b47
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-02 12:18:32 +0300

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,10 +2,7 @@
 let x = 0;
 process.argv.forEach((val, index) => {
   x++;
-  if (index === 2) {
-    console.log(`${val}`);
-  }
-  if (index === 3) {
-    console.log('is');
+  if (index === 2) or (index === 3){
+    console.log(`${val}` + 'is');
   }
 });
