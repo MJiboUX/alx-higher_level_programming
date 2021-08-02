3ee0ad69de3d3875beaec724f5bb394788627ddb
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-02 13:37:53 +0300

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const x = process.argv[2];
 if (isNaN(x) === true) {
-console.log('Not a number');
+  console.log('Not a number');
 } else {
-console.log('My number: ' + x);
+  console.log('My number: ' + x);
 }
