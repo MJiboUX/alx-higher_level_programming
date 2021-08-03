274b148a3f67ac9c0532a7ce491121270f4b2694
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-03 09:57:46 -0700

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-let i = 0;
-const m = list.map(x => x * i++);
-console.log(list);
-console.log(m);
+const array = require('./100-data').list;
+console.log(array);
+console.log(array.map(function (x, i) { return (x * i); }));
