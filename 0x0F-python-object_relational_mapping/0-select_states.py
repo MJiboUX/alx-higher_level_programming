2a5675f6126f9d3df4777e84f43be5c562222014
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-06 12:46:25 +0300

    add """

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ from MySQLdb import _mysql
 
 db = _mysql.connect(user="alx",passwd="1234",db="hbtn_0e_0_usa")
 cursor = db.cursor()
-sql = SELECT * FROM states ORDERBY states.id
+sql = """SELECT * FROM states ORDERBY states.id"""
 try:
     cursor.execute(sql)
     db.commit()
