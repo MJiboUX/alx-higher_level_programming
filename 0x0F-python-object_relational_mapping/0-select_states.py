aa6a935902c30473e5ed9214ebbef48fb0eb2d1d
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-06 12:59:40 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,16 +4,17 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-db = MySQLdb.connect(
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    host="localhost",
-    port=3306)
-cursor = db.cursor()
-sql = """SELECT * FROM states ORDER BY states.id ASC"""
-cursor.execute(sql)
-for row in cursor.fetchall():
-    print(row)
-cursor.close()
-db.close()
+
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306)
+    cursor = db.cursor()
+    sql = """SELECT * FROM states ORDER BY states.id ASC"""
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
