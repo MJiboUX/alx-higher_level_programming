e0c4be625a9d5f73f778e6ac8f44864e19878da8
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-06 14:23:04 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Script takes an argument and displays values """
-import MYSQLdb
+""" Script takes an argument """
+import MySQLdb
 import sys
 
 if __name__ == '__main__':
 
-    db = MYSQLdb.connect(
+    db = MySQLdb.connect(
         user=sys.argv[1],
         password=sys.argv[2],
         database=sys.argv[3],
@@ -14,8 +14,8 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states \
-                    WHERE name LIKE BINARY {:s} \
-                    ORDER BY states.id ASC".format(sys.argv[4]))
+               WHERE name LIKE BINARY '{:s}' \
+               ORDER BY states.id ASC".format(sys.argv[4]))
     for row in cursor.fetchall():
         print(row)
     cursor.close()
