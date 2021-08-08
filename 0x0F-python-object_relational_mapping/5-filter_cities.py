7091f05ef151985358ad153a2781e67996d41ea7
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-07 21:23:52 -0700

    removed modules

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     cursor.execute("SELECT cities.name FROM cities \
                     INNER JOIN states ON \
                     cities.state_id=states.id \
-                    WHERE states.name=%s", % (sys.argv[4],))
+                    WHERE states.name=%s", (sys.argv[4]))
     for i in cursor.fetchall():
         print(i)
     cursor.close()
