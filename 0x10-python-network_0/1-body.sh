44d485a851d27065e42bdb2f3d9457027140f0e8
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-08-19 09:25:16 -0700

    short comment

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
-# display only body of a 200 status code response
-# used curl
+# script that takes in a URL, sends a GET request to the URL
 curl -sLf $1
