13c0bbababbae107928b7599f2a82635d1d92a0b
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-14 14:48:52 +0300

    fix styles error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,14 +2,14 @@
 
 const req = require('request');
 const url = process.argv[2];
-req.get(url, function( err, res, body) {
+req.get(url, function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
-    let data = JSON.parse(body);
+    const data = JSON.parse(body);
     const completed = {};
     for (const i of data) {
-      if (i.completed == true) {
+      if (i.completed === true) {
         if (i.userId in completed) {
           completed[i.userId]++;
         } else {
