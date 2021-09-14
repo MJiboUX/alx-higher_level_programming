d1ca7c38ab36c1cdd2627590a77e267414c115c6
kalkidan999 <kalkidandemes99@gmail.com>
2021-09-14 12:02:20 +0000

    fixINDENT

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,14 +7,15 @@ req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
   }
- const data = JSON.parse(body);
- const dd = data.characters;
- for (const i of dd) {
- req.get(i, function (error, res, body1) {
-   if (error) {
-     console.log(error);
-   }
-   const data1 = JSON.parse(body1);
-     console.log(data1.name);
-   });
-}});
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
+});
