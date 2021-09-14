d984512b45ca2b2773616d2cdb40c0204aa8eb7a
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-14 14:25:33 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const Firstarg = process.argv[2];
 const Secondarg = process.argv[3];
-fs.writeFileSync(Firstarg, Secondarg, "utf-8", function(error, log){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(log);
-    }
+fs.writeFileSync(Firstarg, Secondarg, 'utf-8', function (error, log){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(log);
+  }
 });
