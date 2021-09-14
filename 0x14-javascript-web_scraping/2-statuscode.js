af09b181cc7e6cbe1745948336181efff021e807
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-14 14:28:42 +0300

    fix js rule

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 
 const req = require('request');
 const Firstarg = process.argv[2];
-req.get(Firstarg, function(error,response){
- console.error('error:', error);
- console.log('code:', response && response.statusCode);
+req.get(Firstarg, function (error, response) {
+  console.error('error:', error);
+  console.log('code:', response && response.statusCode);
 });
