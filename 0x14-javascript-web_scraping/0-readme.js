52b8010befd9635e8fd5d8aeafb56b3f6b9c9e76
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-14 14:23:07 +0300

    add space at line5

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const fname = process.argv[2];
-fs.readFile(fname, 'utf-8', function(err, lo) {
+fs.readFile(fname, 'utf-8', function (err, lo) {
   if (err) {
     console.log(err);
   } else {
