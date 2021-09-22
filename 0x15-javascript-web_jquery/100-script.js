0c4acef092d03887c0e5528180fda101953b1b02
kalkidan999 <kalkidandemes99@gmail.com>
2021-09-22 10:35:12 -0700

    DOm

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,6 +1,6 @@
-(function(){
+(function() {
     document.addEventListener('DOMContentLoaded', function() {
         const col = document.querySelector('header');
-        col.style.color('#FF0000');
+        col.style.color = '#FF0000';
     }, false);
 })();
