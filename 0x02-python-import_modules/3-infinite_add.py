bfcf36fea9f18714119e0a94cb0a3d635ccbeee1
Mubarak Mohammad Jibo <mubarrakmohammadjibo@gmail.com>
2023-01-15 02:53:42 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
+
+    ac = len(sys.argv) - 1
     res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
+
+    if ac > 0:
+        for i in range(ac):
+            res += int(sys.argv[i + 1])
     print("{:d}".format(res))
